59573286d03dda592e1c4a6bc0c79dc7b6af3042
Nikolay <halemhood@icloud.com>
2022-02-14 10:34:40 +0300

    Edited

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -7,48 +7,61 @@
 
 import UIKit
 
+enum Lights {
+    case red, yellow, green
+}
+
 class ViewController: UIViewController {
     
     @IBOutlet var redLigth: UIView!
     @IBOutlet var yellowLigth: UIView!
     @IBOutlet var greenLigth: UIView!
+    
     @IBOutlet var changeButton: UIButton!
     
+    private var currentLight: Lights = .red
+    
+    private let lightOff: CGFloat = 0.1
+    private let lightOn: CGFloat = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        redLigth.alpha = 0.1
-        yellowLigth.alpha = 0.1
-        greenLigth.alpha = 0.1
-        redLigth.layer.cornerRadius = 65
-        yellowLigth.layer.cornerRadius = 65
-        greenLigth.layer.cornerRadius = 65
+        
         changeButton.layer.cornerRadius = 15
+        
+        redLigth.alpha = lightOff
+        yellowLigth.alpha = lightOff
+        greenLigth.alpha = lightOff
+        
+
+    }
+    
+    override func viewWillLayoutSubviews() {
+        redLigth.layer.cornerRadius = redLigth.frame.width / 2
+        greenLigth.layer.cornerRadius = greenLigth.frame.width / 2
+        yellowLigth.layer.cornerRadius = yellowLigth.frame.width / 2
+
     }
     
     
     @IBAction func changeLights() {
-        switch redLigth.alpha {
-        case let x where x == 1: redLigth.alpha = 0.1
-            yellowLigth.alpha = 1
-        case let x where x != 1 && yellowLigth.alpha == 1: yellowLigth.alpha = 0.1
-            greenLigth.alpha = 1
-        case let x where x != 1 && greenLigth.alpha == 1: changeButton.setTitle("START", for: .normal)
-            greenLigth.alpha = 0.1
-        case let x where x != 1: changeButton.setTitle("NEXT", for: .normal)
-            redLigth.alpha = 1
-            
-        default: break
-        }
         
-//        if redLigth.alpha != 1 {
-//            changeButton.setTitle("NEXT", for: .normal)
-//            redLigth.alpha = 1
-//        } else {
-//            changeButton.setTitle("START", for: .normal)
-//            redLigth.alpha = 0.1
-//        }
+        changeButton.setTitle("Next", for: .normal)
+        
+        switch currentLight {
+        case .red:
+            greenLigth.alpha = lightOff
+            redLigth.alpha = lightOn
+            currentLight = .yellow
+        case .yellow:
+            redLigth.alpha = lightOff
+            yellowLigth.alpha = lightOn
+            currentLight = .green
+        case .green:
+            yellowLigth.alpha = lightOff
+            greenLigth.alpha = lightOn
+            currentLight = .red
+        }
     }
-
 }
 
